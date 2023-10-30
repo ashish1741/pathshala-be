b2fd62872f33aec0ddf267246bb28f4fb762108b
Ashish poudel <techcode057@gmail.com>
2023-10-30 20:02:11 +0545

    pp completed

--- server.ts
+++ server.ts
@@ -1,5 +1,15 @@
 import {app} from "./app";
 import connectDB from "./utilis/db";
+import {v2 as cloudinary} from "cloudinary"
+
+
+// cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRETE
+
+})
 
 //create server
 app.listen(3000, () =>{
